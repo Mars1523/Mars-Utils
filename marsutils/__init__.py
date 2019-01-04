b8413aa0e4947ea3a3fb36952daa31ab50c8b91a
Noskcaj <kcajdev@gmail.com>
2019-01-03 21:23:48 -0700

    Fix flake message

--- marsutils/__init__.py
+++ marsutils/__init__.py
@@ -3,7 +3,5 @@
 __author__ = """Noskcaj19"""
 __version__ = "0.1.0"
 
-
-from .controlmanager import ControlManager, ControlInterface
-from .decorator import with_ctrl_manager, provide_setup
-
+from .controlmanager import ControlManager, ControlInterface  # noqa: F401
+from .decorator import with_ctrl_manager, provide_setup  # noqa: F401
