11fe15effe6779eaf13f3dd4713fccab10dd8a9f
Noskcaj <kcajdev@gmail.com>
2020-01-06 12:25:02 -0500

    Add doctests to `signed_square`

--- marsutils/math.py
+++ marsutils/math.py
@@ -3,7 +3,13 @@ import math
 
 def signed_square(value):
     """ A tiny function that returns the square of ``value`` but with the
-        same sign, e.g. ``signed_square(-2)`` returns ``-4``
+        same sign.
+
+        >>> signed_square(-2)
+        -4
+
+        >>> signed_square(2)
+        2
     """
     return math.copysign(value * value, value)
 
